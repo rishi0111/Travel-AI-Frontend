0dae999a66f36769a7a0139b089138eca29da508
neha-snake <neha@snakescript.com>
2025-04-03 16:22:42 +0530

    minor changes

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -8,7 +8,7 @@ const Chat = () => {
     <Sidebar />
     {/* Main Content */}
     <div className="flex-1 ms-[275px]">
-        <div className="h-[calc(100vh-95px)] overflow-y-auto ">
+        <div className="h-[calc(100vh-95px)] overflow-y-auto py-[30px]">
             {/* Hero Section */}
             <div className="relative h-[400px] overflow-hidden">
                 <div className="relative z-10 h-full flex flex-col justify-center px-16 text-center">
@@ -24,7 +24,7 @@ const Chat = () => {
             <div className="max-w-[1035px] mx-auto px-[20px]">
                 <div className="">
                     {/* AI Assistant Chat */}
-                    <div className="max-w-[740px]">
+                    <div className="max-w-[740px] mb-[30px]">
                         <div className="flex items-start gap-[8px]">
                             <div className="w-[40px] h-[40px] rounded-full bg-[#E7ECF9] flex items-center justify-center flex-shrink-0">
                                 <img src={AiAssistantIcon} alt="AI Assistant" className="" />
@@ -37,6 +37,13 @@ const Chat = () => {
                                 </p>
                                 <p className="text-xs text-gray-500 mt-2">05:10 AM</p>
                             </div>
+                        </div>
+                    </div>
+                    {/*  User Chat */}
+                    <div className="max-w-[740px] ms-auto mb-[30px]">
+                        <div className="flex items-end flex-col justify-end">
+                           <p className="border border-[#E5E7EB] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)] bg-[#0D3FC6] rounded-[20px] rounded-es-none px-[16px] py-[12px] text-[14px] leading-[20px] text-[#FFFFFF]">Find me a tour package to Bali for 7 days</p>
+                           <p className="text-xs text-gray-500 mt-2">05:10 AM</p>
                         </div>
                     </div>
                 </div>
